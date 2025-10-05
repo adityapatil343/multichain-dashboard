e504aa2c7242d17e1376f2100b86be6478c777bf
Aditya Patil <160331022+adityapatil343@users.noreply.github.com>
2025-10-06 00:55:54 +0530

    Delete src/erc20Abi.ts

--- src/erc20Abi.ts
+++
@@ -1,6 +0,0 @@
-
-export const erc20Abi = [
-  { "type": "function", "name": "balanceOf", "inputs": [{"name": "owner", "type": "address"}], "outputs": [{"name": "", "type": "uint256"}], "stateMutability": "view" },
-  { "type": "function", "name": "decimals", "inputs": [], "outputs": [{"name": "", "type": "uint8"}], "stateMutability": "view" },
-  { "type": "function", "name": "symbol", "inputs": [], "outputs": [{"name": "", "type": "string"}], "stateMutability": "view" }
-] as const
